8c70a76d52adadf977e85117e63454f8a0290a65
harveytriana <harveytriana@hotmail.com>
2021-09-08 16:16:46 -0500

    Factorial

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,24 +6,34 @@ namespace Fibonacci
 {
     class Program
     {
-        static int _i;
+        static void Main() {
 
-        static void Main(string[] args) {
-
-            Console.WriteLine("Number = ? ");
+            Console.Write("Number = ? ");
             _ = long.TryParse(Console.ReadLine(), out long x);
-            if (x > 0)
-            {
-                Console.WriteLine("Fibonacci_Recursive({0}) = {1}", x, Fibonacci(x));
+            if (x > 0 && x <= 20) {
+                Console.WriteLine("\nFibonacci_Recursive({0}) = {1}", x, Fibonacci(x));
+
+                Console.WriteLine("Factorial: {0}", Factorial(x));
+            } else {
+                Console.WriteLine("Bad number");
             }
+
+
+            Console.ReadKey();
         }
 
         static long Fibonacci(long number) {
-            Console.WriteLine($"{++_i}\t{number}");
             return number switch {
                 <= 2 => 1,
                 3 => 2,
-                _ => Fibonacci(number - 1) + Fibonacci(number - 2),
+                _ => Fibonacci(number - 1) + Fibonacci(number - 2)
+            };
+        }
+
+        static long Factorial(long number) {
+            return number switch {
+                < 2 => 1,
+                _ => number * Factorial(number - 1)
             };
         }
     }
