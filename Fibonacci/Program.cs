f5859963075f5a5d18b32cd1fc51383cd9ed5c69
harveytriana <harveytriana@hotmail.com>
2021-09-09 11:59:29 -0500

    Fibonacci BigInteger

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -10,23 +10,23 @@ namespace Fibonacci
         static void Main() {
             Console.WriteLine("Fibionacci Approach");
             while (true) {
-                Console.Write("Number = ? ");
+                Console.Write("\nNumber = ? ");
                 _ = uint.TryParse(Console.ReadLine(), out uint x);
-                if (x > 0 && x <= 1000) {
-                    //Console.WriteLine("\nFibonacci_Recursive({0}) = {1}", x, Fibonacci(x));
-                    Console.WriteLine("Factorial: {0}", Factorial(x));
-                } else {
-                    Console.WriteLine("Bad number");
-                    break;
-                }
+                //if (x > 0 && x <= 1000) {
+                    Console.WriteLine("\nFibonacci_Recursive({0}) = {1}", x, FibonacciIterative(x));
+                    //Console.WriteLine("Factorial: {0}", Factorial(x));
+                //} else {
+                //    Console.WriteLine("Bad number");
+                //    break;
+                //}
             }
         }
 
-        static ulong Fibonacci(ulong number) {
+        static ulong FibonacciApproach(uint number) {
             return number switch {
                 <= 2 => 1,
                 3 => 2,
-                _ => Fibonacci(number - 1) + Fibonacci(number - 2)
+                _ => FibonacciApproach(number - 1) + FibonacciApproach(number - 2)
             };
         }
 
@@ -34,6 +34,14 @@ namespace Fibonacci
             return number switch {
                 < 2 => 1,
                 _ => number * FactorialApproach(number - 1)
+            };
+        }
+
+        static BigInteger Fibonacci(uint number) {
+            return number switch {
+                <= 2 => 1,
+                3 => 2,
+                _ => Fibonacci(number - 1) + Fibonacci(number - 2)
             };
         }
 
@@ -45,7 +53,7 @@ namespace Fibonacci
         }
 
         #region Experiments
-       
+
         public static int FindMaxValue() {// returns 20
             int res = 2;
             long fact = 2;
@@ -59,7 +67,64 @@ namespace Fibonacci
             }
             return res - 1;
         }
+        // fnciona hasta 46
+        public static int FibonacciIterative32(uint number) {
+            int a = 0, b = 1, result = 0;
+            for (int i = 0; i <= number; i++) {
+                if (i <= 1)
+                    result = i;
+                else {
+                    result = a + b;
+                    a = b;
+                    b = result;
+                }
+            }
+            return result;
+        }
 
+        // funciona hasta 92
+        public static long FibonacciIterativeLong(uint number) {
+            long a = 0, b = 1, result = 0;
+            for (uint i = 0; i <= number; i++) {
+                if (i <= 1)
+                    result = i;
+                else {
+                    result = a + b;
+                    a = b;
+                    b = result;
+                }
+            }
+            return result;
+        }
+        // funciona hasta 93
+        public static ulong FibonacciIterativeUlong(uint number) {
+            ulong a = 0, b = 1, result = 0;
+            for (uint i = 0; i <= number; i++) {
+                if (i <= 1)
+                    result = i;
+                else {
+                    result = a + b;
+                    a = b;
+                    b = result;
+                }
+            }
+            return result;
+        }
+
+        // funciona hasta ?
+        public static BigInteger FibonacciIterative(uint number) {
+            BigInteger a = 0, b = 1, result = 0;
+            for (uint i = 0; i <= number; i++) {
+                if (i <= 1)
+                    result = i;
+                else {
+                    result = a + b;
+                    a = b;
+                    b = result;
+                }
+            }
+            return result;
+        }
         #endregion
     }
 }
